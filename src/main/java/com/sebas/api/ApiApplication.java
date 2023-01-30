d35b1b11c16962673a27b8febf3abd483df86dc9
JohamSMC <joham.smc@gmail.com>
2023-01-30 15:19:15 -0500

    [Update] application.properties info health

--- src/main/java/com/sebas/api/ApiApplication.java
+++ src/main/java/com/sebas/api/ApiApplication.java
@@ -2,12 +2,19 @@ package com.sebas.api;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class ApiApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(ApiApplication.class, args);
     }
 
+    @GetMapping()
+    public String getBasePath() {
+        return ("<HTML><body> <a href=\"https://spring-test-production.up.railway.app/actuator\"> Link clik to go paths</a></body></HTML>");
+    }
 }
